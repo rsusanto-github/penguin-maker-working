9a9d4a26f4230c7ca2bc1e60d8ae5fce3c950f7e
Robert Susanto <77759596+rsusanto-github@users.noreply.github.com>
2021-04-20 23:40:44 -0700

    new commit
    Fixed: Blurry Sprites
    Added: More Customizations

--- CompositeSpritesheets/CompositeSprites.gd
+++ CompositeSpritesheets/CompositeSprites.gd
@@ -10,3 +10,22 @@ const hair_spritesheet = {
 	3 : preload("res://CompositeSpritesheets/Hair4.png"),
 	4 : preload("res://CompositeSpritesheets/Hair5.png")
 }
+const eye_spritesheet = {
+	0 : preload("res://CompositeSpritesheets/Eye1.png"),
+	1 : preload("res://CompositeSpritesheets/Eye2.png"),
+	2 : preload("res://CompositeSpritesheets/Eye3.png"),
+	3 : preload("res://CompositeSpritesheets/Eye4.png")
+}
+const clothes_spritesheet = {
+	0 : preload("res://CompositeSpritesheets/Clothes1.png"),
+	1 : preload("res://CompositeSpritesheets/Clothes2.png"),
+	2 : preload("res://CompositeSpritesheets/Clothes3.png"),
+	3 : preload("res://CompositeSpritesheets/Clothes4.png")
+}
+const shoes_spritesheet = {
+	0 : preload("res://CompositeSpritesheets/Shoes1.png"),
+	1 : preload("res://CompositeSpritesheets/Shoes2.png"),
+	2 : preload("res://CompositeSpritesheets/Shoes3.png"),
+	3 : preload("res://CompositeSpritesheets/Shoes4.png"),
+	4 : preload("res://CompositeSpritesheets/Shoes5.png")
+}
--- Penguin.gd
+++ Penguin.gd
@@ -3,18 +3,42 @@ extends Node2D
 onready var bodySprite = $CompositeSprites/Body
 onready var hairSprite = $CompositeSprites/Hair
 onready var eyeSprite = $CompositeSprites/Eye
-onready var clothesrSprite = $CompositeSprites/Clothes
+onready var clothesSprite = $CompositeSprites/Clothes
 onready var shoesSprite = $CompositeSprites/Shoes
 
 const composite_sprites = preload("res://CompositeSpritesheets/CompositeSprites.gd")
 
 var curr_hair: int = 0
+var curr_eye: int = 0
+var curr_clothes: int = 0
+var curr_shoes: int = 0
 func _ready():
 	bodySprite.texture = composite_sprites.body_spritesheet[0]
 	hairSprite.texture = composite_sprites.hair_spritesheet[curr_hair]
+	eyeSprite.texture = composite_sprites.eye_spritesheet[curr_eye]
+	clothesSprite.texture = composite_sprites.clothes_spritesheet[curr_clothes]
+	shoesSprite.texture = composite_sprites.shoes_spritesheet[curr_shoes]
 
 
 func _on_ChangeHair_pressed():
 	curr_hair = (curr_hair+1) % composite_sprites.hair_spritesheet.size()
 	hairSprite.texture = composite_sprites.hair_spritesheet[curr_hair]
+	pass # Replace with function body.
+
+
+func _on_ChangeEyes_pressed():
+	curr_eye = (curr_eye+1) % composite_sprites.eye_spritesheet.size()
+	eyeSprite.texture = composite_sprites.eye_spritesheet[curr_eye]
+	pass # Replace with function body.
+
+
+func _on_ChangeClothes_pressed():
+	curr_clothes = (curr_clothes+1) % composite_sprites.clothes_spritesheet.size()
+	clothesSprite.texture = composite_sprites.clothes_spritesheet[curr_clothes]
+	pass # Replace with function body.
+
+
+func _on_ChangeShoes_pressed():
+	curr_shoes = (curr_shoes+1) % composite_sprites.shoes_spritesheet.size()
+	shoesSprite.texture = composite_sprites.shoes_spritesheet[curr_shoes]
 	pass # Replace with function body.
